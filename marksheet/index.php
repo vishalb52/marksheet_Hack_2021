b68886c8ce51523f0a9fa25b5d9e74443630927f
vishalb52 <60908967+vishalb52@users.noreply.github.com>
2021-02-14 00:51:59 +0530

    Add files via upload

---
+++ marksheet/index.php
@@ -0,0 +1,401 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="bootstrap-4.5.0-dist/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="bootstrap-4.5.0-dist/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+
+	<link rel="stylesheet" href="main.css">
+
+
+
+
+</head>
+
+<body>
+	<div class="container-fluid">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">MarkSheet</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Videos</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Photos</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About Project
+          
+        </a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<a class="dropdown-item" href="https://towardsdatascience.com/a-simple-way-to-analyze-student-performance-data-with-python-cc09c7508c4c" target="_blank">IDEA</a>
+							<a class="dropdown-item" href="https://www.hindawi.com/journals/mse/2016/5309348/" target="_blank">Implementation</a>
+							<a class="dropdown-item" href="http://localhost:8888/tree?" target="_blank">Execution</a>
+						</div>
+						
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="sectionLight">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="jumbotron jumbotron-fluid">
+						<div class="container">
+							<h1 class="display-4">Welcome to Future</h1>
+							<p class="lead">Smart Analysis</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+
+				<div class="col"></div>
+				<div class="col-sm-12 col-md-6">
+					<div id="mainCarousel" class="carousel slide" data-ride="carousel">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img src="imgs/3.jpg" class="d-block w-100" alt="First Slide">
+							</div>
+							<div class="carousel-item">
+								<img src="imgs/8.png" class="d-block w-100" alt="Second Slide">
+								<div class="carousel-caption d-none d-md-block">
+
+
+									<h2 class="tt">Look into Future</h2>
+								</div>
+
+							</div>
+							<div class="carousel-item">
+								<img src="imgs/7.png" class="d-block w-100" alt="Third Slide">
+							</div>
+						</div>
+						<a class="carousel-control-prev" href="#mainCarousel" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#mainCarousel" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</div>
+				<div class="col"></div>
+			</div>
+		</div>
+		<!--Section Two-->
+		<div class="sectionDark">
+			<div class="row">
+				<div class="col-sm-12 col-md-1 offset-md-2">
+					<img class="img-fluid" src="imgs/9.jpg">
+				</div>
+
+				<div class="col-sm-12 col-md-6">
+					<div class="row">
+						<div class="col-sm-12">
+							<p>Examinations are an essential part of education and student lifecycle. They are the tools used to measure the success and effectiveness of the teaching processes and methodologies. Examinations are as stressful for the teachers and administrators for the student community, for different reasons. Students only need to worry about preparation and performance, whereas teachers and administrators have a lot to do on their part. Registration, test creation, security, evaluations, candidate verification and result tabulation are some of the important aspects of effective exam administration. Managing exams at a larger scale can be even more taxing. Traditional examination and result computation system involve huge volumes of answer-paper scripts that become difficult to manage and are vulnerable to risks that include damaged and misplaced answer scripts. The result system also requires manual sorting, evaluation and safeguarding, which contributes to the risk of errors and biases, security concerns and may ultimately cause delays in announcing the results. The solution lies in switching over to a digital and automated student result processing system from the current manual result processing system. Automated digital student mark analysis system are a real boon to educational institutions and testing bodies. From planning and preparing to evaluating, the student examination result processing system, in effect, frees up administrators and teachers who can focus their attention on the other important tasks. </p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12 col-md-9">
+							<p><i>MarkSheet</i> software package exclusively designed for automation marks entry and ranking process. Using this software, you can make fast data entry of marks by choosing entry options like exam wise or subject wise or student wise. You can maintain the data of student’s academic analysis throughout his / her academic tenure in the school and generate the reports like Progress Card/Analytical performance reports, Evaluate to Marks/Grades/top ranker/average comparisons.</p>
+						</div>
+						<div class="col-sm-12 col-md-3">
+							<img class="img-fluid" src="imgs/4.jpg">
+						</div>
+					</div>
+				</div>
+				<div class="col"></div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="coolwrapper">
+						<button type="button" class="btn btn-lg coolbtn" data-toggle="modal" data-target="#exampleModal">Check Marks</button>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+		<div class="sectionLight">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="row">
+						<div class="col-sm-12 col-lg-4">
+							<img class="img-fluid imgc" src="imgs/14.jpg">
+							<div class="row">
+								<div class="col-sm-12">
+									<button type="button" class="btn btn-info btn-lg btn-danger btnc" data-toggle="modal" data-target="#myModal">Digital Analytics</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-12 col-lg-4">
+							<img class="img-fluid imgc" src="imgs/11.png">
+							<div class="row">
+								<div class="col-sm-12">
+									<button type="button" class="btn btn-info btn-lg btn-danger btnc" data-toggle="modal" data-target="#Modal">Marks Prediction</button>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-12 col-lg-4">
+							<img class="img-fluid imgc" src="imgs/10.png">
+							<div class="row">
+								<div class="col-sm-12">
+									<button type="button" class="btn btn-info btn-lg btn-danger btnc" data-toggle="modal" data-target="#vModal">KNN</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<footer class="footer bg-dark">
+			<div class="container-fluid ">
+				<div class="row">
+					<div class="col-sm-12 col-lg-3">
+						<img class="img-fluid fimg" src="imgs/15.jpg">
+					</div>
+					<div class="col-sm-12 col-lg-9 social">
+						<a href="https://facebook.com" target="_blank">
+							<i class="fa fa-facebook-square fa-4x"></i></a>
+						<a href="https://instagram.com" target="_blank">
+							<i class="fa fa-instagram fa-4x"></i></a>
+						<a href="https://plus.google.com" target="_blank">
+							<i class="fa fa-google-plus fa-4x"></i></a>
+						
+					</div>
+				</div>
+			</div>
+		</footer>
+
+
+
+
+
+
+
+
+
+		<!-- Modal -->
+		<!--CoolStuff Modal -->
+		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="CoolStuffLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="contentLabel">Get Exclusive Content</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-sm-12">
+									<img class="img-fluid" src="imgs/15.jpg">
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-12">
+									<div class="modalFormWrapper">
+										<form>
+											<div class="form-group">
+												<label for="forName">What's your Name?</label>
+												<input type="text" class="form-control" id="formName" placeholder="Name">
+											</div>
+											<div class="form-group">
+												<label for="forEmail">Email Adress</label>
+												<input type="text" class="form-control" id="formGEmail" placeholder="Email">
+											</div>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+
+
+	</div>
+
+	<!-- Modal -->
+	<div class="modal fade" id="myModal" role="dialog">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title kk"><i>Demonstration Video</i></h4>
+				</div>
+				<div class="modal-body">
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/jV_VqB4Oi2k" allowfullscreen></iframe>
+					</div>
+					<p>Digital Analytics Fundamentals | Web Analytics For Beginners | Digital Marketing</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal"></button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!--SAE 4 -->
+	<div class="modal fade" id="Modal" role="dialog">
+		<div class="modal-dialog modal-md">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title"><i>Demonstration Video</i></h4>
+				</div>
+				<div class="modal-body">
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/U_oJqcyc0eI" allowfullscreen></iframe>
+					</div>
+					<p>Machine Learning Project End To End Till Deployment</p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal"></button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- SAE 3 -->
+	<div class="modal fade" id="vModal" role="dialog">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title"><i>Demonstration Video</i></h4>
+				</div>
+				<div class="modal-body">
+					<div class="embed-responsive embed-responsive-16by9">
+						<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/R6Jcz1UNz3g" allowfullscreen></iframe>
+					</div>
+					<p>KNN - The K Nearest Neighbour Machine Learning Algorithm - Python Scikit </p>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal"></button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+	<script src="bootstrap-4.5.0-dist/js/bootstrap.min.js"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$(".close").click(function() {
+				$("#myModal").on('hidden.bs.modal', function(e) {
+					$("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
+				});
+
+			});
+
+		});
+
+	</script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$(".close").click(function() {
+				$("#vModal").on('hidden.bs.modal', function(e) {
+					$("#vModal iframe").attr("src", $("#vModal iframe").attr("src"));
+				});
+
+			});
+
+		});
+
+	</script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$(".close").click(function() {
+				$("#Modal").on('hidden.bs.modal', function(e) {
+					$("#Modal iframe").attr("src", $("#Modal iframe").attr("src"));
+				});
+
+			});
+
+		});
+
+	</script>
+	<script>
+		"use strict";
+
+		! function() {
+			var t = window.driftt = window.drift = window.driftt || [];
+			if (!t.init) {
+				if (t.invoked) return void(window.console && console.error && console.error("Drift snippet included twice."));
+				t.invoked = !0, t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"],
+					t.factory = function(e) {
+						return function() {
+							var n = Array.prototype.slice.call(arguments);
+							return n.unshift(e), t.push(n), t;
+						};
+					}, t.methods.forEach(function(e) {
+						t[e] = t.factory(e);
+					}), t.load = function(t) {
+						var e = 3e5,
+							n = Math.ceil(new Date() / e) * e,
+							o = document.createElement("script");
+						o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+						var i = document.getElementsByTagName("script")[0];
+						i.parentNode.insertBefore(o, i);
+					};
+			}
+		}();
+		drift.SNIPPET_VERSION = '0.3.1';
+		drift.load('y3ppieshwx86');
+
+	</script>
+</body>
+
+</html>
